6f0d0985875dc85cc4e61dba8b3bb3447086ca51
centforever <centforever@163.com>
2023-12-02 15:26:46 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        mavenCentral()
+    }
+
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        id("app.cash.sqldelight") version "2.0.0" apply false
+    }
+}
+
+rootProject.name = "AdbTools"
---
+++ src/main/kotlin/util/AdbUtils.kt
@@ -0,0 +1,21 @@
+package util
+
+object AdbUtils {
+    fun formatCommand(adbCommand: String, device: String): String {
+        var adb = adbCommand
+
+        if (device.isNotBlank()) {
+            adb = adbCommand.replace("adb", "adb -s $device")
+        }
+
+        // if windows os
+        if (System.getProperty("os.name").contains("Windows")) {
+            adb = adb.replace("adb", "cmd /c adb").replace("grep", "findstr")
+        }
+        return adb
+    }
+}
+
+fun String.formatAdbCommand(device: String): String {
+    return AdbUtils.formatCommand(this, device)
+}
